7db8d243cbbb23ce5904c8beb0550f588d935bc6
Victor Rajkumar <vrajkumar@informatica.com>
2022-09-11 16:01:38 +0530

    react app for deployment

---
+++ src/components/Photos.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+
+const Photos = () => {
+  const [photos, setPhotos] = React.useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/api/photos")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log("data", data);
+        const firstSet = data.splice(1, 100);
+        // setPhotos((state) => {
+        //   return [...state, ...firstSet];
+        // });
+        setPhotos([...firstSet]);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>Photos List</div>
+      {photos.map((photo) => {
+        return (
+          <div class="card" style={{ width: "18rem" }}>
+            <img src={photo.thumbnailUrl} class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">{photo.title}</h5>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default Photos;
